a2925293c0ff3255293a9790d9fe42ea02dc057a
Patrick <pfbrowning943@yahoo.com>
2018-07-04 13:55:22 -0400

    style(modules): lint and clean up the module files

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  FileDropModule  } from '@browninglogic/ng-file-drop';
+import { FileDropModule } from '@browninglogic/ng-file-drop';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
