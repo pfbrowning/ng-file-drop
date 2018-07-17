dd1f303db77e2bfcf620b9f513c2e8eee367d24b
Patrick Browning <pfbrowning943@yahoo.com>
2018-07-16 23:11:30 -0400

    feat(demo): alert the demo user when a file is rejected

--- src/module/lib.module.ts
+++ src/module/lib.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileInputComponent } from './components/file-input.component';
 
-export { FileInputComponent } from './components/file-input.component';
+export { FileRejection } from './models/file-rejection.model';
+export { RejectionReasons } from './models/rejection-reasons.model';
 
 @NgModule({
   imports: [ CommonModule ],
