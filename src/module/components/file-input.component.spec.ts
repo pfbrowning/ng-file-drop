eb60caeb72ad2a7ca3ecbb2b0b0076f5f35101af
Patrick Browning <pfbrowning943@yahoo.com>
2018-08-01 21:56:06 -0400

    test(tests): add tests for browser drag events

--- src/module/components/file-input.component.spec.ts
+++ src/module/components/file-input.component.spec.ts
@@ -13,6 +13,7 @@ describe('File Input Component', () => {
     let selectionChangedSpy: jasmine.Spy;
     let filesRejectedSpy: jasmine.Spy;
     let dropHandlerDiv;
+    let fileInput;
     let testFile1: File;
     let testFile2: File;
     let testFileJson: File;
@@ -37,6 +38,8 @@ describe('File Input Component', () => {
 
         // Query to find the handler container div
         dropHandlerDiv  = handlerFixture.debugElement.query(By.css('div.nfdDragDropHandler')).nativeElement;
+        // Query for the file input
+        fileInput = handlerFixture.debugElement.query(By.css('input[type=file]')).nativeElement;
 
         // Initialize the spies
         selectionChangedSpy = spyOn(handlerInstance.selectionChanged, 'emit').and.callThrough();
@@ -228,8 +231,14 @@ describe('File Input Component', () => {
         // Check the state of the component before selecting files
         expect(handlerInstance.selectedFiles.length).toBe(0);
         expect(handlerInstance.filesSelected).toBe(false);
+        expect(handlerInstance.dragging).toBe(false);
         expect(selectionChangedSpy).not.toHaveBeenCalled();
 
+        // Simulate the start of the drag
+        dropHandlerDiv.dispatchEvent(new Event('dragenter'));
+
+        // Ensure that the drag state is being set properly
+        expect(handlerInstance.dragging).toBe(true);
 
         // Simulate a file being dropped on the container div
         dropHandlerDiv.dispatchEvent(dropEvent);
@@ -237,9 +246,34 @@ describe('File Input Component', () => {
         // Check the state of the component after file drop
         expect(handlerInstance.selectedFiles.length).toBe(3);
         expect(handlerInstance.filesSelected).toBe(true);
+        expect(handlerInstance.dragging).toBe(false);
         expect(selectionChangedSpy).toHaveBeenCalledTimes(1);
         expect(selectionChangedSpy).toHaveBeenCalledWith([testFile1, testFile2, testFileBig]);
     });
+
+    it('should properly handle a file being dragged over', () => {
+        // Check the state of the dragging flag before doing anything
+        expect(handlerInstance.dragging).toBe(false);
+
+        // Simulate the user starting to drag a file
+        dropHandlerDiv.dispatchEvent(new Event('dragenter'));
+
+        /* The dragover event happens many times during a file drag, 
+        so we'll roughly simulate that here*/
+        for(let i = 0; i < 10; i++) {
+            dropHandlerDiv.dispatchEvent(new Event('dragover'));
+        }
+
+        //The component should be in a dragging state after dragenter
+        expect(handlerInstance.dragging).toBe(true);
+
+        // Simulate the user dragging the file out of the handler div
+        dropHandlerDiv.dispatchEvent(new Event('dragleave'));
+
+        // The component should no longer be in a dragging state after dragleave
+        expect(handlerInstance.dragging).toBe(false);
+
+    })
 });
 
 
